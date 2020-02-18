9e3bed0aca0d8a23cbdc713f197d9018b8c1d881
Gituser143 <bhargavsnv100@gmail.com>
2020-02-18 19:05:01 +0530

    Remove report copies

--- HDFS-Replication-Simulator-Edited/src/hdfs/replicationsimulator/Main.java
+++ HDFS-Replication-Simulator-Edited/src/hdfs/replicationsimulator/Main.java
@@ -25,7 +25,7 @@ public class Main {
 		Simulator.start();
 		
 //		while (!isEnded) wait();
-		Thread.sleep(1200000);
+		Thread.sleep(12000);
 		System.out.println("END");
 		
 		Simulator.printResults();
